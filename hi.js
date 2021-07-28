1459c0ffac2ea4b463cd8ac08cafdf7eef6ab661
Shadow <william.shadow@outlook.com>
2021-07-28 13:50:31 -0500

    Update hi.js (POEditor.com)

--- hi.js
+++ hi.js
@@ -1,1 +1,13 @@
-{}
+{
+    "narratorbot": "Narrator Bot",
+    "ping": "Ping",
+    "boosterDaily": "क्योंकि आप एक बूस्टर हैं, आपको सामान्य पुरस्कार राशि से दोगुना प्राप्त हुआ है!",
+    "daily": "{{emoji}} बढ़िया!  आपको {{number}} {{prize}} मिल गया है!",
+    "noItemProvided": "कृपया खरीदने के लिए दुकान से एक वस्तु निर्दिष्ट करें !",
+    "unknownColor": "क्षमा करें, मैं {{color}} रंग को नहीं पहचानता।  सुनिश्चित करें कि आपने \\`+shop colors\\` में से एक उचित रंग चुना है!",
+    "alreadyPurchasedRole": "अरे, आपने वह भूमिका पहले ही खरीद लिया है!",
+    "alreadyPurchasedColor": "अरे, आपने वह रंग पहले ही खरीद लिया है!",
+    "alreadyPurchasedItem": "अरे, आपने वह {{item}} पहले ही खरीद लिया है!",
+    "notEnoughCurrency": "क्षमा करें, आपके पास इसके लिए पर्याप्त {{currency}} नहीं है!",
+    "channelPurchaseSuccess": "You have purchased a private channel! You can edit your channel here: {{channelLink}}"
+}
