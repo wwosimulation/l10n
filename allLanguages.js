e0fa6e8cad37aa88ae50731edad82b1551ce5ac1
Shadow <william.shadow@outlook.com>
2021-07-28 13:11:48 -0500

    Create allLanguages.js

---
+++ allLanguages.js
@@ -0,0 +1,9 @@
+module.exports = []
+const languageFiles = fs.readdirSync(__dirname).filter((file) => file.endsWith(".json"))
+for (const file of languageFiles) {
+    module.exports.push(`${file.split(`.`).shift()}`)
+}
+Object.filter = (obj, predicate) =>
+    Object.keys(obj)
+        .filter((key) => predicate(obj[key]))
+        .reduce((res, key) => ((res[key] = obj[key]), res), {})
